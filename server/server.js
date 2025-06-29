6da96fdcbf814e345373c2137134594a6a2756a4
Hardik-N-dotcom <hrdknarayan@gmail.com>
2025-06-30 00:33:35 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ import authRoutes from './routes/auth.js';
 
 dotenv.config();
 const app = express();
+const port = process.env.PORT || 5000 
 
 app.use(express.json());
 app.use(cookieParser());
@@ -19,5 +20,5 @@ app.use(cors({
 app.use('/api/auth', authRoutes);
 
 mongoose.connect(process.env.URI)
-  .then(() => app.listen(5000, () => console.log('✅ Server running at http://localhost:5000')))
+  .then(() => app.listen(port, () => console.log('✅ Server running at http://localhost:5000')))
   .catch(err => console.log(err));
